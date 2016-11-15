50b801121b9a845c4ae081a2c3a4415bc2ae496f
Stephan White <sgradywhite@yahoo.com>
2016-11-15 16:04:11 +0000

    fix the host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'enigmatic-citadel-19717.herokuapp.com'
+  host = 'tranquil-bastion-51261.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
